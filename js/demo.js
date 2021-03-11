e40be0145ca99763087d0dee7b283467d726028f
Hugo Barsacq <58510166+hugobarsacq@users.noreply.github.com>
2021-03-12 00:44:32 +0100

    Add files via upload

---
+++ js/demo.js
@@ -0,0 +1,94 @@
+/**
+ * demo.js
+ * http://www.codrops.com
+ *
+ * Licensed under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ * 
+ * Copyright 2017, Codrops
+ * http://www.codrops.com
+ */
+{
+	setTimeout(() => document.body.classList.add('render'), 60);
+	const navdemos = Array.from(document.querySelectorAll('nav.demos > .demo'));
+	const total = navdemos.length;
+	const current = navdemos.findIndex(el => el.classList.contains('demo--current'));
+	const navigate = (linkEl) => {
+		document.body.classList.remove('render');
+		document.body.addEventListener('transitionend', () => window.location = linkEl.href);
+	};
+	navdemos.forEach(link => link.addEventListener('click', (ev) => {
+		ev.preventDefault();
+		navigate(ev.target);
+	}));
+	document.addEventListener('keydown', (ev) => {
+		const keyCode = ev.keyCode || ev.which;
+		let linkEl;
+		if ( keyCode === 37 ) {
+			linkEl = current > 0 ? navdemos[current-1] : navdemos[total-1];
+		}
+		else if ( keyCode === 39 ) {
+			linkEl = current < total-1 ? navdemos[current+1] : navdemos[0];
+		}
+		else {
+			return false;
+		}
+		navigate(linkEl);
+	});
+}function animateFrom(elem, direction) {
+	direction = direction | 1;
+	
+	var x = 0,
+		y = direction * 100;
+	if(elem.classList.contains("gs_reveal_fromLeft")) {
+	  x = -100;
+	  y = 0;
+	} else if(elem.classList.contains("gs_reveal_fromRight")) {
+	  x = 100;
+	  y = 0;
+	}
+	gsap.fromTo(elem, {x: x, y: y, autoAlpha: 0}, {
+	  duration: 1.25, 
+	  x: 0,
+	  y: 0, 
+	  autoAlpha: 1, 
+	  ease: "expo", 
+	  overwrite: "auto"
+	});
+  }
+  
+  function hide(elem) {
+	gsap.set(elem, {autoAlpha: 0});
+  }
+  
+  document.addEventListener("DOMContentLoaded", function() {
+	gsap.registerPlugin(ScrollTrigger);
+	
+	gsap.utils.toArray(".gs_reveal").forEach(function(elem) {
+	  hide(elem); // assure that the element is hidden when scrolled into view
+	  
+	  ScrollTrigger.create({
+		trigger: elem,
+		onEnter: function() { animateFrom(elem) }, 
+		onEnterBack: function() { animateFrom(elem, -1) },
+		onLeave: function() { hide(elem) } // assure that the element is hidden when scrolled into view
+	  });
+	});
+  });
+
+  gsap.utils.toArray(".slideText").forEach(text => {
+	gsap.timeline({
+	  defaults: {ease: "none"},
+	  scrollTrigger: {
+		scroller: text.closest(".horizSlider"),
+		horizontal: true,
+		trigger: text.closest(".slide"),
+		start: "left right",
+		end: "left left",
+		scrub: true
+	  }
+	})
+	.fromTo(text, {x: 250}, {x: -250}, 0)
+	.from(text.nextElementSibling, {scale: 0.8}, 0)
+  });
+  
